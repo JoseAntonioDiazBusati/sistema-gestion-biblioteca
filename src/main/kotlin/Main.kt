58f8d985d36139829459aae30ca89f6522e4561c
JoseAntonio <jdiabus511@g.educaand.es>
2025-02-21 09:22:18 +0100

    ejercicio terminado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,16 @@
 package org.example
 
+import org.example.DatosBiblioteca.DVD
+import org.example.DatosBiblioteca.Libro
+import org.example.DatosBiblioteca.Revista
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    val libro = Libro("El señor de los anillos: El retorno del Rey", "Yo",2025)
+    val libro1 = Libro("El padrino", "No es anonimo",1920)
+    val libro2 = Libro("Harry Potta 3", "Yo",2025)
 
+    val profesor = Usuario.Profesor("0007", "Diegoool", "Programacion")
+    println(profesor.tomarPrestado(profesor,libro))
 }
--- src/main/kotlin/Usuario.kt
+++ src/main/kotlin/Usuario.kt
@@ -1,9 +1,19 @@
 package org.example
 
+import org.example.DatosBiblioteca.Libro
+
 sealed class Usuario {
 
-    data class Estudiante(val id: String, val nombre: String, val carrera: String)
-    data class Profesor(val id: String, val nombre: String, val departamento: String)
-    data class Visitante(val id: String, val nombre: String)
+    data class Estudiante(val id: String, val nombre: String, val carrera: String): Usuario()
+    data class Profesor(val id: String, val nombre: String, val departamento: String): Usuario()
+    data class Visitante(val id: String, val nombre: String): Usuario()
+
+    fun tomarPrestado(usuario: Usuario, libro: Libro): String {
+        return when (usuario) {
+            is Estudiante -> "${usuario.nombre} puede tomar prestado '${libro.titulo}' por 10 días."
+            is Profesor -> "${usuario.nombre} puede tomar prestado '${libro.titulo}' por 20."
+            is Visitante -> "${usuario.nombre} no puede tomar prestado '${libro.titulo}'."
+        }
+    }
 
 }
